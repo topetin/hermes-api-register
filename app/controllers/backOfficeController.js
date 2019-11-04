d6a430d9fda571890125f0ff5e15cf07c06fd597
unknown <carballalmariana@gmail.com>
2019-11-03 23:09:30 -0300

    add users

--- app/controllers/backOfficeController.js
+++ app/controllers/backOfficeController.js
@@ -31,7 +31,50 @@ backofficeController.listUsers = async (req, res) => {
         .catch((error) => responseHandler.serverError(res, error))
     
     db.listUsers(authId)
-    .then((result) => responseHandler.send200(res, result.length === 0 ? [] : result[0]))
+    .then((result) => responseHandler.send200(res, result.length === 0 ? [] : result))
+    .catch((error) => responseHandler.serverError(res, error))
+}
+
+backofficeController.addUsers = async (req, res) => {
+    const token = req.headers['authorization']
+
+    if (!token) return responseHandler.missingToken(res);
+
+    const authId = await verifyToken(token)
+        .then((auth) => { return auth.id })
+        .catch((error) => responseHandler.serverError(res, error))
+    
+    const users = req.body.users;
+
+    let emailList = [];
+    for (let i = 0; i< users.length; i++) {
+        let obj = Object.assign({}, users[i]);
+        emailList.push(obj.email);
+    }
+    
+    db.isAvailableEmail(emailList)
+    .then((result) => {
+        if (result.length > 0) {
+            let existentEmails = []
+            for (let i = 0; i< result.length; i++) {
+                let obj = Object.assign({}, result[i]);
+                existentEmails.push(obj.email);
+            }
+            return responseHandler.send403(res, existentEmails)
+        }
+        let values = []
+        for (let i = 0; i < users.length; i++) {
+            let obj = Object.assign({}, users[i]);
+            let reg = []
+            Object.keys(obj).forEach(item => {reg.push(obj[item])})
+            reg.push(generateUsername(obj.email))
+            reg.push(authId)
+            values.push(reg)
+        }
+        db.addUsers(values)
+        .then((result2) => responseHandler.send200(res, true))
+        .catch((error2) => responseHandler.serverError(res, error))
+    })
     .catch((error) => responseHandler.serverError(res, error))
 }
 
@@ -43,6 +86,10 @@ const verifyToken = async (token) => {
             return resolve(user);
         })
     })
+}
+
+const generateUsername = (email) => {
+    return email.split('@')[0];
 }
 
 module.exports = backofficeController;
--- app/repository/mysql/backofficeQueries.js
+++ app/repository/mysql/backofficeQueries.js
@@ -10,4 +10,12 @@ backOfficeQueries.listUsers = (companyId) => {
     return db.query(`SELECT * FROM user WHERE company_id = ? and company_id <> id`, [companyId]);
 }
 
+backOfficeQueries.isAvailableEmail = (emails) => {
+    return db.query(`SELECT * FROM user WHERE email IN(?)`, [emails]);
+}
+
+backOfficeQueries.addUsers = (values) => {
+    return db.query(`INSERT INTO user (name, email, role_id, username, company_id ) VALUES ?`, [values])
+}
+
 module.exports = backOfficeQueries
